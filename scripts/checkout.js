957fb64b78d22868c92f4022cdf07d3ac001b493
AbdullahTh <abdullah.t.younis@gmail.com>
2024-06-13 19:30:07 +0300

    Delete products from HTML when clickimg delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,7 +9,7 @@ cart.forEach(cartItem =>{
     let matchingProduct = products.find(product => product.id === productId);
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productId}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -94,6 +94,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=> {
     link.addEventListener('click', () => {
         const productId = link.dataset.productId; 
         deleteProduct(productId);
-        console.log(cart);
+        
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
     })
 })
