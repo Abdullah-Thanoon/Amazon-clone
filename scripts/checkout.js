70452c01b227847b72a891dd768a5bb2154f84ca
AbdullahTh <abdullah.t.younis@gmail.com>
2024-06-22 17:23:06 +0300

    Complete test files

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,5 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import {cart} from '../data/cart.js'
 
-export function updateNavbar() {
-    let counter = 0;
-    cart.forEach(element => {
-        counter += element.quantity;
-    });
-    
-    document.querySelector(".return-to-home-link").innerText = counter + ' items';
-}
-
-updateNavbar();
 renderOrderSummary();
 renderPaymentSummary();
